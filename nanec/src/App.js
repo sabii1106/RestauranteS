b57530d48851e16d962dd62b6f6a3c0a7f3cfedd
Mathew EPN <mathew.gutierrez@epn.edu.ec>
2025-05-22 15:22:53 -0500

    feat: Initialize React application with essential files and configurations
    - Added logo512.png for application branding.
    - Created manifest.json for PWA support with app icons and display settings.
    - Included robots.txt to manage web crawler access.
    - Established App.css for styling the main application component.
    - Developed App.js as the main component with a logo and a link to learn React.
    - Implemented App.test.js for basic testing of the App component.
    - Set up index.css for global styles.
    - Configured index.js to render the App component and report web vitals.
    - Added logo.svg for the application logo.
    - Created reportWebVitals.js for performance measurement.
    - Set up setupTests.js for testing environment configuration.

--- nanec/src/App.js
+++ nanec/src/App.js
@@ -2,13 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Restaurante from './Componentes/Restaurante';
 const urlImagen1="https://static.abc.es/media/familia/2018/05/23/Minevera_Pasta-k4KC--1240x698@abc.jpg";
-const restaurantes = [{nombre:"El Redil", direccion:"Shyris y Matamoros", tipo:"Tradicional", urlImagen:urlImagen1},{nombre:"Cafeteria", direccion:"Julio Cesar Villacres y N68F", tipo:"Cafeteria", urlImagen:"https://static.abc.es/media/familia/2018/05/23/Minevera_Pasta-k4KC--1240x698@abc.jpg"}, {nombre:"Restaurante los Pedrosa", direccion:"Presa y Zamora", tipo:"Comida Rápida", urlImagen:"https://static.abc.es/media/familia/2018/05/23/Minevera_Pasta-k4KC--1240x698@abc.jpg"}]
+const restaurantes = [{nombre:"El Redil", direccion:"Shyris y Matamoros", tipo:"Tradicional", urlImagen:"https://static.abc.es/media/familia/2018/05/23/Minevera_Pasta-k4KC--1240x698@abc.jpg"},{nombre:"Cafeteria", direccion:"Julio Cesar Villacres y N68F", tipo:"Cafeteria", urlImagen:"https://static.abc.es/media/familia/2018/05/23/Minevera_Pasta-k4KC--1240x698@abc.jpg"}, {nombre:"Restaurante los Pedrosa", direccion:"Presa y Zamora", tipo:"Comida Rápida", urlImagen:"https://static.abc.es/media/familia/2018/05/23/Minevera_Pasta-k4KC--1240x698@abc.jpg"}];
 function App() {
     return (
     <div className="App">
       {
-        restaurantes.map((restaurante)=>{
-          <Restaurante nombre = {restaurante.nombre} direccion = {restaurante.direccion} tipo = {restaurante.tipo} urlImagen={restaurante.urlImagen}></Restaurante>
+        restaurantes.map((restaurante,index)=>{
+          return <Restaurante key = {index} nombre = {restaurante.nombre} direccion = {restaurante.direccion} tipo = {restaurante.tipo} urlImagen={restaurante.urlImagen}></Restaurante>
         })
       }
     </div>
--- nanec/src/Componentes/Restaurante.jsx
+++ nanec/src/Componentes/Restaurante.jsx
@@ -1,16 +1,33 @@
 import React from "react";
 import "./Restaurante.css";
 class Restaurante extends React.Component{
-    
+    //El constructor solo se ejecuta una vez al inicio, y es para dar valor a las variables de estado
+    constructor(props){
+        super(props);
+        this.state={
+            //Variables de estado llamada likes
+            likes: 0,          
+        }
+        this.handlerLike = this.handlerLike.bind(this); //Se usa para que el this dentro de la funcion se refiera a la clase Restaurante
+    }
+
+    //handlerLike(){
+        //this.state.likes = this.state.likes + 1;  //Forma incorrecta de modificar el estado
+        //this.setState({likes: this.state.likes + 1}); //Forma correcta de modificar el estado
+    //}
+    handlerLike = () => this.setState({likes: this.state.likes + 1}); 
     render(){
 
         const {nombre, direccion, tipo, UrlImagen}= this.props;
         return (
             <div className="Resta">
+                <img src={UrlImagen} alt="" />
                 <h1>{nombre}</h1>
                 <h3>{direccion}</h3>
                 <h4>{tipo}</h4>
-                <img src={UrlImagen} alt="" />
+                <h2>{this.state.likes}</h2>
+                <button onClick={this.handlerLike}>Like</button>
+                
             </div>  
         );      
     }
