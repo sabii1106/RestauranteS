0f02b3a6871af5945e269436adbb8c69aaa79661
Mathew EPN <mathew.gutierrez@epn.edu.ec>
2025-05-26 14:49:55 -0500

    feat: Update Restaurante component with like/dislike functionality and styling

--- nanaec_/nanec_funcional/src/App.js
+++ nanaec_/nanec_funcional/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+
 import Restaurante from './Componentes/Restaurante';
 const urlImagen1="https://static.abc.es/media/familia/2018/05/23/Minevera_Pasta-k4KC--1240x698@abc.jpg";
 const restaurantes = [{nombre:"El Redil", direccion:"Shyris y Matamoros", tipo:"Tradicional", urlImagen:"https://static.abc.es/media/familia/2018/05/23/Minevera_Pasta-k4KC--1240x698@abc.jpg"},{nombre:"Cafeteria", direccion:"Julio Cesar Villacres y N68F", tipo:"Cafeteria", urlImagen:"https://static.abc.es/media/familia/2018/05/23/Minevera_Pasta-k4KC--1240x698@abc.jpg"}, {nombre:"Restaurante los Pedrosa", direccion:"Presa y Zamora", tipo:"Comida Rápida", urlImagen:"https://static.abc.es/media/familia/2018/05/23/Minevera_Pasta-k4KC--1240x698@abc.jpg"}];
--- nanaec_/nanec_funcional/src/Componentes/Restaurante.jsx
+++ nanaec_/nanec_funcional/src/Componentes/Restaurante.jsx
@@ -1,21 +1,38 @@
 import React, {useState} from "react";
 function Restaurante (props){
-
-    handlerLike = () => setLikes(likes + 1); //Forma correcta de modificar el estado
-    
     const [likes, setLikes] = useState(0);
-    
-    
+    const [dislikes, setDislikes] = useState(0);
     const {nombre, direccion, tipo, UrlImagen}= props;
+    const [preferencias, setPreferencias] = useState({
+        likes: 0,
+        dislikes: 0
+    });
+    //Podría ocurrir que esta linea no funcione, dado que 
+    //react busca la optimización, y solo renderiza cuando
+    //es necesario. Espera varias operaciones antes de renderizar
+    //
+    const handlerLike = () =>{
+        //setLikes(likes + 1); //Forma correcta de modificar el estado
+        setLikes(prevLikes =>{
+            return (prevLikes + 1) //Forma correcta de modificar el estado, usando el valor previo
+            }
+        );
+    }
+    
+    const handlerDislike = () => {
+        setDislikes(prevDislikes => prevDislikes - 1);
+    }
+    
     return (
-        <div className="Resta">
+        <div className="Restaurante">
             <img src={UrlImagen} alt="" />
             <h1>{nombre}</h1>
             <h3>{direccion}</h3>
             <h4>{tipo}</h4>
-            <h4>{likes}</h4>
-            
-            <button onClick={handlerLike}>Like</button>
+            <h4>Me gusta:{likes}</h4>
+            <h4>No me gusta:{dislikes}</h4>
+            <button onClick={handlerLike}>👍</button>
+            <button onClick={handlerDislike}>👎</button>
             
         </div>  
     );      
