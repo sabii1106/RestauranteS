3b5e381ef9426f0ddc2dba8e5a8dc087c7f15b25
Mathew EPN <mathew.gutierrez@epn.edu.ec>
2025-06-04 16:01:55 -0500

    feat: Implement routing and add Inicio component for restaurant application

--- nanaec_/nanec_funcional/src/App.js
+++ nanaec_/nanec_funcional/src/App.js
@@ -1,15 +1,24 @@
-import BrowserRouter from 'react-dom';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import ListaRestaurantes from './Componentes/ListaRestaurantes';
-
+import CrearRestaurante from './Componentes/CrearRestaurante';
+import Inicio from './Componentes/Inicio';
 function App() {
     
   return (
     <div className="App">
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element = {<Inicio/>}/>    
+        <Route path={"/lista"} element = {<ListaRestaurantes/>}/>    
+        <Route path={"/crear"} element = {<CrearRestaurante/>}/>    
+      </Routes>
+    </BrowserRouter>
 
-    <ListaRestaurantes />
     </div>
   );
 }
 
 export default App;
+
---
+++ nanaec_/nanec_funcional/src/Componentes/Inicio.jsx
@@ -0,0 +1,14 @@
+
+
+function Incio() {
+    
+  return (
+    <div className="Inicio">
+    <h1>
+        Bienvenido a la Aplicación de Restaurantes
+    </h1>
+    </div>
+  );
+}
+
+export default Incio;
