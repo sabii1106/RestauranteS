4c44bd35ed259b65ebc989f871597e63d32f1db1
Mathew <mathew.gutierrez@epn.edu.ec.com>
2025-06-05 13:24:39 -0500

    feat: Refactor routing and add WrapperRestaurante component for restaurant management

--- nanaec_/nanec_funcional/src/App.js
+++ nanaec_/nanec_funcional/src/App.js
@@ -1,21 +1,17 @@
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import ListaRestaurantes from './Componentes/ListaRestaurantes';
-import CrearRestaurante from './Componentes/CrearRestaurante';
+import WrapperRestaurante from './Componentes/WrapperRestaurante';
 import Inicio from './Componentes/Inicio';
+
 function App() {
-    
   return (
     <div className="App">
-    <BrowserRouter>
-      <Routes>
-        <Route path={"/"} element = {<Inicio/>}/>    
-        <Route path={"/lista"} element = {<ListaRestaurantes/>}/>    
-        <Route path={"/crear"} element = {<CrearRestaurante/>}/>    
-      </Routes>
-    </BrowserRouter>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/*" element={<WrapperRestaurante />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ nanaec_/nanec_funcional/src/Componentes/WrapperRestaurante.jsx
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import ListaRestaurantes from "./ListaRestaurantes";
+import CrearRestaurante from "./CrearRestaurante";
+
+function WrapperRestaurante() {
+  const [restaurantes, setRestaurantes] = useState([
+    {
+      nombre: "El Redil",
+      direccion: "Shyris y Matamoros",
+      tipo: "Tradicional",
+      reputacion: 1,
+      UrlImagen:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkGTV9ptpoJ1nv8SE8QJ_A4-pCjnd46axWiA&s",
+    },
+    {
+      nombre: "Cafeteria",
+      direccion: "Julio Cesar Villacres y N68F",
+      tipo: "Cafeteria",
+      reputacion: 2,
+      UrlImagen:
+        "https://th.bing.com/th/id/OIP.HXhKQnE0QlerZWTn5b27_gHaE8?w=274&h=183&c=7&r=0&o=5&pid=1.7",
+    },
+    {
+      nombre: "Restaurante los Pedrosa",
+      direccion: "Presa y Zamora",
+      tipo: "Comida Rápida",
+      reputacion: 5,
+      UrlImagen:
+        "https://th.bing.com/th/id/OIP.Lq4y_ACmvY-av-0cmcxQeQHaE7?w=287&h=191&c=7&r=0&o=5&pid=1.7",
+    },
+  ]);
+  const [mensajeErrorLikesNegativos, setMensajeErrorLikesNegativos] =
+    useState("");
+  const [likesTotales, setLikesTotales] = useState(0);
+  const [state, setState] = useState({
+    nombre: "",
+    direccion: "",
+    tipo: "",
+    reputacion: "",
+    UrlImagen: "",
+  });
+
+  const agregarRestaurantes = (nuevoRestaurante) => {
+    setRestaurantes((prev) => [...prev, nuevoRestaurante]);
+  };
+
+  const SumarLikes = () => setLikesTotales((prev) => prev + 1);
+
+  const RestarDislikes = () => {
+    if (likesTotales <= 0) {
+      mensajeErrorLikesNegativo("No se puede restar más likes");
+      return;
+    }
+    setLikesTotales((prev) => prev - 1);
+  };
+
+  const mensajeErrorLikesNegativo = (mensaje) => {
+    setMensajeErrorLikesNegativos(mensaje);
+    setTimeout(() => setMensajeErrorLikesNegativos(""), 3000);
+  };
+
+  return (
+    <Routes>
+      <Route
+        path="/lista"
+        element={
+          <ListaRestaurantes
+            restaurantes={restaurantes}
+            SumarLikes={SumarLikes}
+            RestarDislikes={RestarDislikes}
+            mensajeErrorLikesNegativo={mensajeErrorLikesNegativo}
+            mensajeErrorLikesNegativos={mensajeErrorLikesNegativos}
+            likesTotales={likesTotales}
+          />
+        }
+      />
+      <Route
+        path="/crear"
+        element={
+          <CrearRestaurante
+            state={state}
+            setState={setState}
+            agregarRestaurante={agregarRestaurantes}
+          />
+        }
+      />
+    </Routes>
+  );
+}
+
+export default WrapperRestaurante;
+
